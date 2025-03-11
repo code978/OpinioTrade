10508b4c1fca796f3e6f956d6ee0b325501b4dd5
kunal755 <kunal@troopr.io>
2025-03-11 22:50:32 +0530

    add real-time updates for events and trades on socket connection

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const http = require('http');
 const socketio = require('socket.io');
 const app = require('./app');
 const Event = require('./models/Event.model');
+const Trade = require('./models/Trade.model');
 
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log('New client connected');
-  
+
+  // Update events in real-time
+  Event.watch().on('change', (data) => {
+    if (data.operationType === 'update') {
+      emitEventUpdate(data.documentKey._id);
+    }
+  });
+
+  // Update trades in real-time
+  Trade.watch().on('change', (data) => {
+    if (data.operationType === 'update') {
+      emitEventUpdate(data.documentKey.event);
+    }
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
